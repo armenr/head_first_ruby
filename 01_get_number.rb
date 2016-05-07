71a0cb2fe7538091e647b4a5e90fe6370ea60d2f
Armen Rostamian <arostamian@ulive.com>
2016-05-06 21:00:51 -0700

    play with escape character & single quote

--- 01_get_number.rb
+++ 01_get_number.rb
@@ -1,9 +1,15 @@
 # Get My Number Game
 # Written by: An Ass-Hat IV
 
+# Greet our friend & get their name
 puts "Welcome to 'this simplistic exercise of a game!'"
 print "What's your name? "
-
-input = gets
-
+input = gets.chomp
 puts "Welcome, #{input}"
+
+# Store a random number for the player to guess
+puts 'I\'ve got a random number between 1 and 100.'
+puts 'Can you guess it?'
+
+# +1 to bring us into 1-100 range, instead of 0-99
+target = rand(100) + 1
