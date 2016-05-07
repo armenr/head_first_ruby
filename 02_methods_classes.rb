917d67119b8be312180cbe723cd658e00383198f
Armen Rostamian <arostamian@ulive.com>
2016-05-07 14:47:22 -0700

    parameters vs arguments

--- 02_methods_classes.rb
+++ 02_methods_classes.rb
@@ -20,17 +20,24 @@ use_headlights("high-beam")
 
 
 
-## Optional parameters sandbox/exercises
-## Here, flavor is a default requirement, the others have default values set
-# def order_soda(flavor, size = "medium", quantity = 1)
-#   if quantity == 1
-#     plural = "soda"
-#   else
-#     plural = "sodas"
-#   end
-#   puts "#{quantity} #{size} #{flavor} #{plural}, coming right up!"
-# end
-#
-# order_soda("orange")
-# order_soda("lemon-lime", "small", 2)
-# order_soda("grape", nil, 5)
+# Optional parameters sandbox/exercises
+# Here, flavor is a default requirement, the others have default values set
+def order_soda(flavor, size = "medium", quantity = 1)
+  if quantity == 1
+    plural = "soda"
+  else
+    plural = "sodas"
+  end
+  puts "#{quantity} #{size} #{flavor} #{plural}, coming right up!"
+end
+
+order_soda("orange")
+order_soda("lemon-lime", "small", 2)
+order_soda("grape", nil, 5)
+
+# Arguments VS Parameters
+def say_wassup(name) # <--- parameter
+  puts "Wassup #{name}!" #<--- parameter
+end
+
+say_wassup("Mofo") #<--- here "Mofo" is an argument
